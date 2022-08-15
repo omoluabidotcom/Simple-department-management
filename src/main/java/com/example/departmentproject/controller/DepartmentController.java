6730091b98de2f7304da312286426985fcdbbd8e
omoluabidotcom <yyahaya222@yahoo.com>
2022-08-15 15:13:50 +0100

    Get request to fetch all depatment in database complete

--- src/main/java/com/example/departmentproject/controller/DepartmentController.java
+++ src/main/java/com/example/departmentproject/controller/DepartmentController.java
@@ -3,9 +3,12 @@ package com.example.departmentproject.controller;
 import com.example.departmentproject.entity.Department;
 import com.example.departmentproject.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class DepartmentController {
@@ -17,5 +20,11 @@ public class DepartmentController {
     public Department addDepartment(@RequestBody Department department) {
 
         return departmentService.saveDepartment(department);
+    }
+
+    @GetMapping("/department")
+    public List<Department> fetchDepartment() {
+
+        return departmentService.getDepartment();
     }
 }
--- src/main/java/com/example/departmentproject/service/DepartmentService.java
+++ src/main/java/com/example/departmentproject/service/DepartmentService.java
@@ -2,8 +2,11 @@ package com.example.departmentproject.service;
 
 import com.example.departmentproject.entity.Department;
 
+import java.util.List;
+
 public interface DepartmentService {
 
     public Department saveDepartment(Department department);
 
+    public List<Department> getDepartment();
 }
