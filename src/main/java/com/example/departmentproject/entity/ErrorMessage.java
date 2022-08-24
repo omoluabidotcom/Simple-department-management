acce2e30a84d749189419244e085ca6139562f0c
omoluabidotcom <yyahaya222@yahoo.com>
2022-08-24 16:03:47 +0100

    Exception handling for find department by id yet to work

---
+++ src/main/java/com/example/departmentproject/entity/ErrorMessage.java
@@ -0,0 +1,15 @@
+package com.example.departmentproject.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorMessage {
+
+    public HttpStatus status;
+    public String message;
+}
