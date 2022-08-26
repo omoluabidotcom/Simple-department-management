6bedc6f6223b9dcbff34a794836b4ede4230913d
omoluabidotcom <yyahaya222@yahoo.com>
2022-08-26 04:01:53 +0100

    Unit test for service layer findbydepartmentname add

--- src/main/java/com/example/departmentproject/entity/Department.java
+++ src/main/java/com/example/departmentproject/entity/Department.java
@@ -1,6 +1,9 @@
 package com.example.departmentproject.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,6 +13,9 @@ import javax.validation.constraints.NotBlank;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Department {
 
     @Id
---
+++ src/test/java/com/example/departmentproject/service/DepartmentServiceTest.java
@@ -0,0 +1,48 @@
+package com.example.departmentproject.service;
+
+import com.example.departmentproject.entity.Department;
+import com.example.departmentproject.repository.DepartmentRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class DepartmentServiceTest {
+
+    @Autowired
+    DepartmentService departmentService;
+
+    @MockBean
+    DepartmentRepository departmentRepository;
+
+    @BeforeEach
+    void setUp() {
+
+        Department department = Department.builder()
+                .departmentAddress("Garki")
+                .departmentCode("HR-05")
+                .departmentName("HR")
+                .departmentId(1L)
+                .build();
+
+        Mockito.when(departmentRepository.findByDepartmentName("HR"))
+                .thenReturn(department);
+
+    }
+
+    @Test
+    //@DisplayName("When name is valid")
+    public void whenDepartmentNameValid_DepartmentShouldFound() {
+
+        String departmentName = "HR";
+        Department found = departmentService.getDepartmentByName(departmentName);
+
+        assertEquals(departmentName, found.getDepartmentName());
+    }
+}
