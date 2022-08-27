145796789030a4c5d95bf6cce5c1247c40da2259
omoluabidotcom <yyahaya222@yahoo.com>
2022-08-27 18:40:13 +0100

    unit test for controller later addDepartment is done

---
+++ src/test/java/com/example/departmentproject/controller/DepartmentControllerTest.java
@@ -0,0 +1,65 @@
+package com.example.departmentproject.controller;
+
+import com.example.departmentproject.entity.Department;
+import com.example.departmentproject.service.DepartmentService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(DepartmentController.class)
+class DepartmentControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private DepartmentService departmentService;
+
+    private Department department;
+
+    @BeforeEach
+    void setUp() {
+
+        department = Department.builder()
+                .departmentName("Engineering")
+                .departmentAddress("Area 10")
+                .departmentCode("EG-09")
+                .build();
+
+    }
+
+    @Test
+    void addDepartment() throws Exception {
+
+        Department inputDepartment = Department.builder()
+                .departmentName("Engineering")
+                .departmentAddress("Area 10")
+                .departmentCode("EG-09")
+                .build();
+
+        Mockito.when(departmentService.saveDepartment(inputDepartment))
+                .thenReturn(department);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/department")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\n" +
+                        "  \"departmentName\":\"Engineering\",\n" +
+                        "  \"departmentAddress\":\"Area 10\",\n" +
+                        "  \"departmentCode\":\"EG-09\"\n" +
+                        "}"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    void getEachDepartment() {
+    }
+}
